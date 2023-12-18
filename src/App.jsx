b7796d003e42b828dd74b593e6377d5cfc678a43
Imon <contact@imonweb.co.uk>
2023-12-18 00:41:44 +0000

    reactjs tasktrek

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import "./App.scss"
+import TaskForm from './components/TaskForm'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <TaskForm />
+    
+      <main className='app_main'>
+        <section className="task_column">Section 1</section>
+        <section className="task_column">Section 2</section>
+        <section className="task_column">Section 3</section>
+      </main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Tag.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Tag.scss'
+
+const Tag = () => {
+  return (
+    <button className="tag">HTML</button>
+  )
+}
+
+export default Tag
